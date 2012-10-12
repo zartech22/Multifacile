4b98a9c481caca387697dc05f211995c4a49acc4
Kevin Plestan <keke-222@hotmail.fr>
2012-10-12 23:39:50 +0200

    mise en place du nouveau theme et debut dimplementation de nouvelle fonctionnalites

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 #include <QTranslator>
 #include <QLocale>
 #include <QLibraryInfo>
+#include <QFile>
+#include <QString>
+#include <QMessageBox>
+#include <QProcess>
+#include <QWidget>
+#include <QTextEdit>
+#include <QtDebug>
 #include "fenetre_principale.h"
 
 int main(int argc, char* argv[])
@@ -12,9 +19,40 @@ int main(int argc, char* argv[])
     translator.load("qt_fr");
     app.installTranslator(&translator);
 
+    QFile file(":/css/Multifacile.css");
+
+    file.open(QIODevice::ReadOnly | QIODevice::Text);
+
+    app.setStyleSheet(file.readAll());
+
+    file.close();
+
+    for(int i = 0; i < argc; i++)
+    {
+        QString arg(argv[i]);
+
+        if(arg == "-updated")
+        {
+            int answer = QMessageBox::information(0, "Mise à jour achevé", "Multifacile vient d'être mis à jour. Voulez-vous voir les changements ?", QMessageBox::No | QMessageBox::Yes);
+
+            if(answer == QMessageBox::Yes)
+            {
+                QWidget *change = new QWidget;
+
+                QFile fichier("change.txt");
+                fichier.open(QIODevice::ReadOnly | QIODevice::Text);
+
+                QTextEdit *textEdit = new QTextEdit(fichier.readAll(), change);
+
+                fichier.close();
+
+                change->show();
+            }
+        }
+    }
 
     Fenetre_principale fenetre;
-    fenetre.setWindowIcon(QIcon("icon.png"));
+    fenetre.setWindowIcon(QIcon(":/icon/icone.png"));
     fenetre.show();
 
     return app.exec();
